3f7fa45f5607045f4e7e4f6e91f939c2e14bdee2
YuriyK6181 <tx6181@mail.ru>
2022-11-04 12:53:01 +0700

    Add Work 23 Files

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+from views.product import products_app
+
+app = Flask(__name__)
+
+app.config.update(
+    ENV="development",
+    SECRET_KEY="lvbobvoadvludhlvoinrelhlrblivelovblruwev",
+)
+
+app.register_blueprint(products_app, url_prefix="/products")
+
+@app.route("/")
+def hello_world():
+    # return "<h1>Hello World!</h1>"
+    return render_template("base.html")
+
+def print_request():
+    print("request:", request, ", method=", request.method, ", path=", request.path)
+
+@app.route("/hello/")  # In Flask Add / to the end of route
+@app.route("/hello/<name>/")
+def hello_view(name: str = None):
+    print_request()
+    if name is None:
+        name = request.args.get("name", "")  # If expected only 1 arg with same name
+    name = name.strip()
+    if not name:
+        name="World"
+    # names = request.args.getlist("name") # If expected more than 1 arg with same name
+
+    # return {"message": "Hello!", "name": name, "names": names}
+    return {"message": f"Hello {name}!"}
+
+@app.route("/items/<int:item_id>/")
+def get_item(item_id: int):
+    return {"Item": {"id": item_id}, }
+
+
+@app.route("/items/<item_id>/")
+def get_item_str(item_id: str):
+    return {"Item": {"id": item_id}, }
+
+
+# @app.route("/hello/<name>/")
+# def hello_name(name:str):
+#     return {"message": f"Hello {name}!"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
