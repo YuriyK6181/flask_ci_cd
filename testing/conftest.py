67c9a2cdcad526e39ef848442923ecb924f7cfd7
YuriyK6181 <tx6181@mail.ru>
2022-11-04 16:18:39 +0700

    Fix pylint warnings

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,9 +1,16 @@
+"""
+Doc String
+"""
 from flask.testing import FlaskClient
 from _pytest.fixtures import fixture
 from main import app
 
+
 @fixture
 def client() -> FlaskClient:
+    """
+    Doc String
+    """
     app.config.update(SERVER_NAME="myserver.org")
     with app.test_client() as client:
         with app.app_context():
--- testing/test_main.py
+++ testing/test_main.py
@@ -1,7 +1,14 @@
+"""
+Doc String
+"""
 from flask import url_for
 import random
 
+
 def test_get_item(client):
+    """
+    Doc String
+    """
     random_id = random.randint(1, 100)
     url = url_for("get_item", item_id=random_id)
     response = client.get(url)
