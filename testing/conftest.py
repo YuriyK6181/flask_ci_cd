ee9f679c12655258bf7e21b92085e0307527a5ee
YuriyK6181 <tx6181@mail.ru>
2022-11-04 16:12:32 +0700

    Test get_item with Flask

---
+++ testing/conftest.py
@@ -0,0 +1,10 @@
+from flask.testing import FlaskClient
+from _pytest.fixtures import fixture
+from main import app
+
+@fixture
+def client() -> FlaskClient:
+    app.config.update(SERVER_NAME="myserver.org")
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
---
+++ testing/test_main.py
@@ -0,0 +1,9 @@
+from flask import url_for
+import random
+
+def test_get_item(client):
+    random_id = random.randint(1, 100)
+    url = url_for("get_item", item_id=random_id)
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.json['Item']['id'] == random_id
